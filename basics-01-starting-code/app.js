b8690af99a8408e501de0e2fd07bd0ce3d61aac9
Tanya Shukla <stanya97@gmail.com>
2021-10-01 11:18:50 +0530

    binding attribute with v-bind directive and 'methods'

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,12 +1,24 @@
 // we wanna control the html part with the vue app and we do thi by using vue which is a globally available object
 // it creates a vue app.
 const app =Vue.createApp({
-data: function(){
-    return{
-        courseGoal:'finish the course'
-        //anything we return from there we can use that in our html part.
-    };
-}
+    data: function(){
+        return{
+            courseGoal:'finish the course',
+            vueLink:'https://cli.vuejs.org/config/'
+            //anything we return from there we can use that in our html part.
+        };
+    },
+    methods:{
+        outputGoal(){
+            const randomNumber = Math.random();
+            if(randomNumber<0.5){
+                return 'Learn Vue!!';
+            }else{
+                return 'Master Vue!!';
+            }
+        }
+    }
+
 });
 // now we want to let vue know which part of this html code
 // should be controlled by the vue app
